a6ef6629569fa86e383c5196154ad7a8c16565f3
tedbracht <1580108+flyingdutch20@users.noreply.github.com>
2018-11-23 19:10:00 +0000

    explore traverse index

--- src/wrac_harvester/core.clj
+++ src/wrac_harvester/core.clj
@@ -13,3 +13,20 @@
 (def rb-index (-> (client/get "https://www.runbritainrankings.com/results/resultslookup.aspx") :body parse as-hickory))
 
 (-> (s/select (s/child (s/id :cphBody_dgMeetings) s/first-child) rb-index))
+
+(defn retrieve-urls
+  "Returns a vector of maps for each sub-category on the current category"
+  [tree]
+  (let [links (s/select
+               (s/child
+                (s/id :cphBody_dgMeetings) s/first-child) rb-index)]
+    (vec
+     (map
+      #(hash-map :date (-> (first %) :tag :td :content first :content first)
+                 :url  (-> (nth % 4) :attrs :href)
+                 :name (-> (nth % 2) :content first :content first))
+      links))))
+
+(retrieve-urls rb-index)
+
+(s/select (s/child (s/id :cphBody_dgMeetings) s/first-child) rb-index)
