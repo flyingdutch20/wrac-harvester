d1a0f5798f27160076eac4a960d97fe44f1f9f02
tedbracht <1580108+flyingdutch20@users.noreply.github.com>
2018-12-07 12:55:21 +0000

    spec extras

--- src/wrac_harvester/extras.clj
+++ src/wrac_harvester/extras.clj
@@ -6,5 +6,7 @@
 [runners]
 (filter #(re-find #"1" (:pos %)) runners))
 
-
-
+;Find first male/female
+;what if there are more than one races.
+;first count the number of :pos "1". If only 1 then find find first male and first female
+;(for [runner runners :while (= (:sex runner) "M"))
