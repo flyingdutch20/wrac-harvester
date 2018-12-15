c3c93df42ee5c6e0accf163ed5ecf0ec34f916fb
ted bracht <1580108+flyingdutch20@users.noreply.github.com>
2018-12-15 09:39:01 +0000

    New version 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject wrac-harvester "0.1.0"
+(defproject wrac-harvester "0.2.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
